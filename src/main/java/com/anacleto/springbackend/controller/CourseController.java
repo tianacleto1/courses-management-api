850d7af5b570b4386469aa7af314f6c3d8e14b26
Tiago Silva <tianacleto1@gmail.com>
2023-10-31 17:56:32 -0300

    Implemented entity validation/logic deletion

--- src/main/java/com/anacleto/springbackend/controller/CourseController.java
+++ src/main/java/com/anacleto/springbackend/controller/CourseController.java
@@ -4,10 +4,15 @@ import com.anacleto.springbackend.model.Course;
 import com.anacleto.springbackend.repository.CourseRepository;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Positive;
 import java.util.List;
 
+@Validated
 @RestController
 @RequestMapping("/api/courses")
 public class CourseController {
@@ -24,19 +29,22 @@ public class CourseController {
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<Course> getCourseById(@PathVariable Long id) {
+    public ResponseEntity<Course> getCourseById(@PathVariable @NotNull @Positive Long id) {
         return courseRepository.findById(id)
                             .map(ResponseEntity::ok)
                             .orElse(ResponseEntity.notFound().build());
     }
 
     @PostMapping
-    public ResponseEntity<Course> createCourse(@RequestBody Course course) {
+    public ResponseEntity<Course> createCourse(@RequestBody @Valid Course course) {
         return ResponseEntity.status(HttpStatus.CREATED).body(this.courseRepository.save(course));
     }
 
     @PutMapping("/{id}")
-    public ResponseEntity<Course> updateCourse(@PathVariable Long id, @RequestBody Course course) {
+    public ResponseEntity<Course> updateCourse(
+            @PathVariable @NotNull @Positive Long id,
+            @RequestBody @Valid Course course
+    ) {
         return this.courseRepository.findById(id)
                                     .map(courseFound -> {
                                         courseFound.setName(course.getName());
@@ -48,7 +56,7 @@ public class CourseController {
     }
 
     @DeleteMapping("/{id}")
-    public ResponseEntity<Void> deleteCourse(@PathVariable Long id) {
+    public ResponseEntity<Void> deleteCourse(@PathVariable @NotNull @Positive Long id) {
         return this.courseRepository.findById(id)
                                     .map(courseFound -> {
                                         this.courseRepository.deleteById(courseFound.getId());
--- src/main/java/com/anacleto/springbackend/model/Course.java
+++ src/main/java/com/anacleto/springbackend/model/Course.java
@@ -1,11 +1,19 @@
 package com.anacleto.springbackend.model;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.Where;
+import org.hibernate.validator.constraints.Length;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
 
 @Entity
 @Table(name = "course_tb")
+@SQLDelete(sql = "UPDATE course_tb SET status = 'Inactive' WHERE id = ?")
+@Where(clause = "status = 'Active'")
 public class Course {
 
     @Id
@@ -13,11 +21,23 @@ public class Course {
     @JsonProperty("_id")
     private Long id;
 
-    @Column(length = 200, nullable = false)
+    @NotBlank
+    @NotNull
+    @Length(min = 5, max = 100)
+    @Column(length = 100, nullable = false)
     private String name;
 
+    @NotNull
+    @Length(max = 10)
+    @Pattern(regexp = "Back-end|Front-end")
     @Column(length = 10, nullable = false)
     private String category;
+
+    @NotNull
+    @Length(max = 10)
+    @Pattern(regexp = "Active|Inactive")
+    @Column(length = 10, nullable = false)
+    private String status = "Active";
 
     public Long getId() {
         return id;
@@ -41,5 +61,13 @@ public class Course {
 
     public void setCategory(String category) {
         this.category = category;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
     }
 }
